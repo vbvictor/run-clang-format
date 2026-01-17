45aee05f7a9f2d12ca34ed6a9d7906bb205b7116
Baranov Victor <bar.victor.2002@gmail.com>
2026-01-17 22:42:14 +0300

    feat: add basic script (#6)

---
+++ run-clang-format.py
@@ -0,0 +1,12 @@
+#!/usr/bin python3
+
+import asyncio
+
+async def main() -> None:
+    pass
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
